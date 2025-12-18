62fa856b79e9f10cb39811a630d5e8129b95f2aa
Tessa Power <hello@tessapower.xyz>
2025-12-18 16:32:58 -0600

    Create Ray class

---
+++ include/ray/ray.h
@@ -0,0 +1,25 @@
+#ifndef RAY_H_
+#define RAY_H_
+
+#include <glm/glm.hpp>
+
+class ray {
+ public:
+  ray() = default;
+
+  ray(const glm::vec3& origin, const glm::vec3& direction)
+    : _origin(origin), _direction(direction) {}
+
+  auto origin() const -> const glm::vec3& { return _origin; }
+  auto direction() const -> const glm::vec3& { return _direction; }
+  
+  auto at(float t) const -> glm::vec3 {
+    return _origin + t *  _direction;
+  }
+
+ private:
+  glm::vec3 _origin{};
+  glm::vec3 _direction{};
+};
+
+#endif
