4ee8b178ee2c822e3ce2220d5138070f47ce1ec8
Tessa Power <hello@tessapower.xyz>
2026-01-01 14:36:52 -0600

    Convert color helper fn to use auto declaration

--- include/utils/color.h
+++ include/utils/color.h
@@ -14,7 +14,8 @@ using color = glm::vec3;
  * @param pixel_color The color to write (expects RGB components in [0,1]
  * range using color.r, color.g, and color.b members).
  */
-void write_color(std::vector<unsigned char>& image, int idx, const color& pixel_color) {
+auto write_color(std::vector<unsigned char>& image, int idx,
+  const color& pixel_color) -> void {
   auto r = pixel_color.r;
   auto g = pixel_color.g;
   auto b = pixel_color.b;
