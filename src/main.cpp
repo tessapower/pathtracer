91dbe1f70df57e7899eac7c3c962413dce82d0c4
Tessa Power <hello@tessapower.xyz>
2026-01-01 14:33:16 -0600

    Use Color helper function in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
+#include "utils/color.h"
 
 auto main() -> int {
   // Image
@@ -18,18 +19,10 @@ auto main() -> int {
     std::clog << "\rScanlines remaining: " << (image_height - row)
               << ' ' << std::flush;
     for (auto col = 0; col < image_width; ++col) {
-      auto r = double(col) / (image_width - 1);
-      auto g = double(row) / (image_height - 1);
-      auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      int idx = (row * image_width + col) * channels;
-      image[idx++] = ir;
-      image[idx++] = ig;
-      image[idx] = ib;
+      const auto pixel_color = color(double(col) / (image_width - 1),
+                               double(row) / (image_height - 1), 0);
+      const auto idx = (row * image_width + col) * channels;
+      write_color(image, idx, pixel_color);
     }
   }
 
