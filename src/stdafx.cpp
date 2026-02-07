8538d942318cb7dbaa0779f3f6100c09cffa8fc7
Tessa Power <hello@tessapower.xyz>
2026-02-07 17:27:54 -0600

    Add precompiled header file

---
+++ src/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp - Precompiled header source file
+// Creates the precompiled header from include/stdafx.h
+
+#include "stdafx.h"
+
+// This file must exist but can be empty
+// The compiler uses it to generate the .pch file
