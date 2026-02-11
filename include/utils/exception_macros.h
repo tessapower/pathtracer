de41ec586c8087a14f6dee2c401d3cf60936ccce
Tessa Power <hello@tessapower.xyz>
2026-02-11 16:26:43 -0600

    Create exception macros for better debug messages

---
+++ include/utils/exception_macros.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "core/dx12_info_queue.h"
+
+#include <sstream>
+#include <stdexcept>
+
+// TODO: introduce custom pathtracer error type to replace std::runtime_error
+
+#define DX12_CHECK(hr_call)                                                    \
+    do                                                                         \
+    {                                                                          \
+        HRESULT hr = (hr_call);                                                \
+        if (FAILED(hr))                                                        \
+        {                                                                      \
+            std::ostringstream oss;                                           \
+            oss << "DX12 Error in " << __FILE__ << ":" << __LINE__             \
+                << "\nFunction: " << #hr_call << "\nHRESULT: 0x" << std::hex   \
+                << hr;                                                         \
+            throw std::runtime_error(oss.str());                               \
+        }                                                                      \
+    } while (0)
+
+#define DX12_CHECK_MSG(hr_call, info_queue)                                    \
+    do                                                                         \
+    {                                                                          \
+        (info_queue).Mark();                                                   \
+        HRESULT hr = (hr_call);                                                \
+        if (FAILED(hr))                                                        \
+        {                                                                      \
+            std::ostringstream oss;                                            \
+            oss << "DX12 Error in " << __FILE__ << ":" << __LINE__             \
+                << "\nFunction: " << #hr_call << "\nHRESULT: 0x" << std::hex   \
+                << hr << std::dec;                                             \
+            auto msgs = (info_queue).GetMessages();                            \
+            if (!msgs.empty())                                                 \
+            {                                                                  \
+                oss << "\n\nDebug Layer Messages:\n";                          \
+                for (const auto& msg : msgs)                                   \
+                {                                                              \
+                    oss << "  - " << msg << "\n";                              \
+                }                                                              \
+            }                                                                  \
+            throw std::runtime_error(oss.str());                               \
+        }                                                                      \
+    } while (0)\
+
